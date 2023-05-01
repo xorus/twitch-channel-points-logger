b5a3062c2a12d7083728e5f2397afb555ebf7934
The Xorus <jdpepi@gmail.com>
2023-05-01 20:05:10 +0200

    Refactor watcher.ts to handle missing channel points image and tooltip for channels that do not customize their channel points rewards.

--- src/watcher.ts
+++ src/watcher.ts
@@ -1,6 +1,6 @@
-import { escapeRegExp } from "./lib.js";
-import { log } from "./log.js";
-import { setPoints } from "./store.js";
+import {escapeRegExp} from "./lib.js";
+import {error, log} from "./log.js";
+import {setPoints} from "./store.js";
 
 const TOOLTIP_WAIT_DELAY_MS = 300;
 const INITIAL_POINTS_DELAY_MS = 10 * 1000; // first fetch
@@ -10,14 +10,40 @@ export class Watcher {
     private currencyNameRegex?: string;
     private currentPointCount?: string;
 
-    computePointCount() {
+    private findButton() {
         const image = document.querySelector<HTMLImageElement>('button .channel-points-icon__image');
+        log("could not find channel points image");
         if (!image) return undefined;
         // the currency name is written in the image alt attribute, which will be useful to match via regex later
         this.currencyNameRegex = escapeRegExp(image.alt);
 
         const btn = image.closest<HTMLButtonElement>('button');
         if (!btn || !btn.parentElement) return undefined;
+
+        return btn;
+    }
+
+    computePointCount() {
+        let btn: HTMLButtonElement | null | undefined = undefined;
+
+        const image = document.querySelector<HTMLImageElement>('button .channel-points-icon__image');
+        // log("could not find channel points image");
+        if (image) {
+            // the currency name is written in the image alt attribute, which will be useful to match via regex later
+            this.currencyNameRegex = escapeRegExp(image.alt);
+            btn = image.closest<HTMLButtonElement>('button');
+        } else {
+            btn = document.querySelector<HTMLButtonElement>('[data-test-selector="community-points-summary"] button');
+            // this case is a bit shitty because the default currency name is dependent on locale,
+            // "Channel Points" in English, "Points de chaîne" in French, etc.
+            this.currencyNameRegex = ".+";
+            // this can create a false match if another tooltip starts with a number
+        }
+        // if (!btn || !btn.parentElement) return undefined;
+        if (!btn || !btn.parentElement) {
+            error("could not find channel points button");
+            return undefined;
+        }
 
         // fake a mouse over to open the tooltip (the tooltip description does not have screen-reader accessibility I could tap into :-) )
         // this will allow us to get the exact channel point count
@@ -32,18 +58,23 @@ export class Watcher {
             for (let el of document.querySelectorAll<HTMLDivElement>('.ReactModal__Content')) {
                 el.style.display = "none";
                 // find the point count, it will always be in a tooltip in the format "XXX Currency Name"
-                let regex = new RegExp(`>?\s*([\\d. ',]+) ${this.currencyNameRegex}`, "i");
-                let matches = el.innerHTML.match(regex);
+                // french locale uses an "NNBSP" number separator ("4 840 Points de chaîne" ????)
+                let regex = new RegExp(`^([\\d. ',\u00A0\u2000-\u200B\u202F]+) ${this.currencyNameRegex}`, "i");
+                console.log(regex)
+                let matches = el.innerText.match(regex);
                 if (matches && matches[1]) {
                     // normalize point count into something more number-ish
                     // twitch uses different separators depending on user locale, so we just remove everything that's not a number from the string
                     this.currentPointCount = matches[1].replace(/[^\d]/g, '');
-                    log("Current channel point count =", this.currentPointCount, el.innerHTML);
+                    log("Current channel point count =", this.currentPointCount/*, el.innerHTML*/);
                     setPoints(this.currentPointCount);
                     break;
                 }
             }
-            btn.parentElement?.dispatchEvent(new MouseEvent('mouseout', {
+            if (this.currentPointCount === undefined) {
+                log("could not find channel point tooltip");
+            }
+            btn!.parentElement?.dispatchEvent(new MouseEvent('mouseout', {
                 'view': unsafeWindow,
                 'bubbles': true,
                 'cancelable': true
